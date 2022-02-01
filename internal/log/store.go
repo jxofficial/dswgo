f5f2ffd431557282b1d7d6db66026040f8ed43ca
Keng Jun Xian <junxian.keng@foodpanda.com>
2022-02-01 15:07:14 +0800

    add store methods and tests

--- internal/log/store.go
+++ internal/log/store.go
@@ -7,22 +7,29 @@ import (
 	"sync"
 )
 
+// Record refers to RecordData + RecordLength (8 bytes).
+// Size and length is used interchangeably.
+// RecordData refers to only the raw data.
+
 var (
 	enc = binary.BigEndian
 )
 
 const (
-	lenNumBits = 8
+	lenNumBytes = 8
 )
 
-// store contains two methods to append and read bytes to and from the file
+// store implements two methods to append and read bytes to and from the file
 type store struct {
 	file *os.File
 	mu   sync.Mutex
 	buf  *bufio.Writer // we write to buffered writer instead of file to reduce system calls.
-	size uint64
+	size uint64        // size is the entire size of the file, ie the length of all records
 }
 
+// Append writes the bytes in p into the store.
+// It returns num bytes written (inclusive of record length),
+// the position which we started appending, and error if exists.
 func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -31,6 +38,7 @@ func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
 
 	// Write the length of the record into the buffer so that
 	// when we read, we know how many bytes to read.
+	// record length is written in big endian encoding.
 	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
 		return 0, 0, err
 	}
@@ -41,31 +49,57 @@ func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
 		return 0, 0, err
 	}
 
-	nn += lenNumBits
+	nn += lenNumBytes
 	s.size += uint64(nn)
-	// return num bytes written, position which we started appending, and error if exists
 	return uint64(nn), pos, nil
 }
 
+// Read returns the record data stored at the given position given a pos.
+// pos is the index at which the record starts.
 func (s *store) Read(pos uint64) ([]byte, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
+
 	// flush the buffer to prevent the case where we read a record that the buffer has not flushed to disk
 	if err := s.buf.Flush(); err != nil {
 		return nil, err
 	}
-	size := make([]byte, lenNumBits)
-	// read size of record from file, where size is a slice of bytes
+
+	size := make([]byte, lenNumBytes)
+	// read record size from file, where size is a slice of bytes
 	if _, err := s.file.ReadAt(size, int64(pos)); err != nil {
 		return nil, err
 	}
 	// convert the size, represented as slice of bytes, into int64
-	b := make([]byte, enc.Uint64(size))
-	// read record from file into b (byte slice)
-	if _, err := s.file.ReadAt(b, int64(pos+lenNumBits)); err != nil {
+	recordData := make([]byte, enc.Uint64(size))
+
+	// read record from file into recordData (byte slice)
+	if _, err := s.file.ReadAt(recordData, int64(pos+lenNumBytes)); err != nil {
 		return nil, err
 	}
-	return b, nil
+	return recordData, nil
+}
+
+// ReadAt reads len(p) bytes into p starting from the given offset in the store's file.
+// It returns the number of bytes n read into p, if n < len(p), an error will be returned.
+// It implements io.ReaderAt.
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.file.ReadAt(p, off)
+}
+
+// Close persists any data before closing the file.
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return err
+	}
+	return s.file.Close()
 }
 
 func newStore(f *os.File) (*store, error) {
---
+++ internal/log/store_test.go
@@ -0,0 +1,115 @@
+package log
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	recordData = []byte("hello world")
+	// recordLen is length of entire record, including the len prefix.
+	recordLen = uint64(len(recordData)) + lenNumBytes
+)
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	// test that our service (store implementation) resumes reading from the latest record on service failure
+	s, err = newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(recordData)
+		require.NoError(t, err)
+		require.Equal(t, recordLen*i, pos+n)
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+	for i := uint64(1); i < 4; i++ {
+		rd, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, recordData, rd)
+		pos += recordLen
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		// read length into lenbs
+		lenbs := make([]byte, lenNumBytes)
+		n, err := s.ReadAt(lenbs, off) // n should be 8 bytes
+		require.NoError(t, err)
+		require.Equal(t, lenNumBytes, n)
+
+		off += int64(n)
+
+		// get size of record data by converting the bytes in lenbs to uint64
+		lenRecordData := enc.Uint64(lenbs)
+		rd := make([]byte, lenRecordData)
+		n, err = s.ReadAt(rd, off)
+		require.NoError(t, err)
+		require.Equal(t, recordData, rd)
+		require.Equal(t, int(lenRecordData), n)
+
+		off += int64(n)
+	}
+}
+
+func TestStoreClose(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_close_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	_, _, err = s.Append(recordData)
+	require.NoError(t, err)
+
+	f, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close()
+	require.NoError(t, err)
+
+	// check that store persists the data left in the buffer when Close is called
+	_, afterSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, afterSize > beforeSize)
+}
+
+func openFile(name string) (file *os.File, size int64, err error) {
+	f, err := os.OpenFile(
+		name,
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, 0, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+	return f, fi.Size(), nil
+}
