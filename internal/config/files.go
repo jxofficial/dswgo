e609fc90c527c2457f8694cca10d31d01f9b35c6
Keng Jun Xian <junxian.keng@foodpanda.com>
2022-04-10 18:47:08 +0800

    add foundation for TLS mutual auth
    create CA and server certs

---
+++ internal/config/files.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+)
+
+var (
+	CAFile         = configFile("ca.pem")
+	ServerCertFile = configFile("server.pem")
+	ServerKeyFile  = configFile("server-key.pem")
+)
+
+func configFile(fileName string) string {
+	if dir := os.Getenv("CONFIG_DIR"); dir != "" {
+		return filepath.Join(dir, fileName)
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Join(homeDir, ".proglog", fileName)
+}
